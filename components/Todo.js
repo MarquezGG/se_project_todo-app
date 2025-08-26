6be56e94fbedf38910a0729e07a40e85a6a15ef9
Eduard <edu.petrosian@gmail.com>
2025-08-26 02:15:26 -0400

    )

---
+++ components/Todo.js
@@ -0,0 +1,66 @@
+class Todo {
+  constructor(data, selector, handleCheck, handleDelete) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+    this._handleCheck = handleCheck;
+    this._handleDelete = handleDelete;
+  }
+
+  _setEventListeners() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._handleDelete(this._completed);
+      this._remove();
+    });
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._toggleCompletion();
+      this._handleCheck(this._completed);
+    });
+  }
+
+  _generateCheckboxEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  _generateDateEl() {
+    const todoDate = this._todoElement.querySelector(".todo__date");
+    const dueDate = new Date(this._data.date);
+    if (!isNaN(dueDate)) {
+      todoDate.textContent = `Due: ${dueDate.toLocaleString("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      })}`;
+    } else {
+      todoDate.textContent = "";
+    }
+  }
+
+  _toggleCompletion() {
+    this._data.completed = !this._data.completed;
+    this._completed = this._data.completed;
+  }
+
+  _remove() {
+    this._todoElement.remove();
+  }
+  getView() {
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    this._todoNameEl.textContent = this._data.name;
+    this._generateCheckboxEl();
+    this._setEventListeners();
+    this._generateDateEl();
+    return this._todoElement;
+  }
+}
+
+export default Todo;
